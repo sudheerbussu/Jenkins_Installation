a8748367df5b58456924412c6ae22b0b0ae1a989
sudheerbussu <116156029+sudheerbussu@users.noreply.github.com>
2024-11-27 19:28:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,18 @@
 # Install Openjdk Java 17 
  sudo apt install -y openjdk-17-jdk
 
-# Install Jenkins with bellow commands 
- sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+# Install Jenkins with bellow commands--------
 
- echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
  sudo apt-get update
-
+ 
+#Installing jenkins------------
  sudo apt-get install -y jenkins
 
 # Start Jenkins application using systemctl
